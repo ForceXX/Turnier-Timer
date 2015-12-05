8958d6b7d2c144ecae4aea6049bf89125a1ae607
ForceXX <forcexx@yahoo.de>
2015-12-05 18:09:55 +0100

    Initial upload#

---
+++ MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Erstellt mit SharpDevelop.
+ * Benutzer: ForceXX
+ * Datum: 24.07.2015
+ * Zeit: 12:43
+ * 
+ * Sie können diese Vorlage unter Extras > Optionen > Codeerstellung > Standardheader ändern.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Countdown
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Erstellt mit SharpDevelop.
+ * Benutzer: ForceXX
+ * Datum: 24.07.2015
+ * Zeit: 12:43
+ * 
+ * Sie können diese Vorlage unter Extras > Optionen > Codeerstellung > Standardheader ändern.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace Countdown
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
